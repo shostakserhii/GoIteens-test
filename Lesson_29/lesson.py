29147c4d0171cec2b903d5795ff61a09da16ad34
Serhii Shostak <shostakserhii94@gmail.com>
2022-04-23 13:32:54 +0300

    lesson 29

---
+++ Lesson_29/lesson.py
@@ -0,0 +1,38 @@
+#
+# own_range(start, end, step)
+
+# range(10)   start=0 end=10 step=1
+
+
+def own_range(start=0, end=None, step=1):
+    if end is None:
+        end = start
+        start = 0
+
+    if end < start and step > 0:
+        return None
+
+    if end > start and step < 0:
+        return None
+
+    # if end > start:
+    #     while start < end:
+    #         yield start
+    #         start += step
+    #
+    # elif end < start:
+    #     while start > end:
+    #         yield start
+    #         start += step
+
+    while start != end:
+        yield start
+        if start + (end % step) == end:
+            return
+        start += step
+
+
+x = own_range(0, -11, -2)
+for i in x:
+    print(i)
+# own_range(10, -10, 1)
